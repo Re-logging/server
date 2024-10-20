4ee6d4e0909ff1e40c7d0abbc3184051a083b0a6
sinryuji <siler2474@gmail.com>
2024-10-20 19:56:47 +0900

    add: admin controller add @Qualfier annotation

--- src/main/kotlin/com/relogging/server/controller/AdminController.kt
+++ src/main/kotlin/com/relogging/server/controller/AdminController.kt
@@ -10,6 +10,7 @@ import com.relogging.server.service.plogging.PloggingEventService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -24,7 +25,9 @@ import org.springframework.web.multipart.MultipartFile
 @RequestMapping("/api/admin")
 @Tag(name = "Admin", description = "admin 컨트롤러")
 class AdminController(
+    @Qualifier("newsArticleCrawlingService")
     private val newsArticleCrawlingService: CrawlingService,
+    @Qualifier("ploggingEventCrawlingService")
     private val ploggingEventCrawlingService: CrawlingService,
     private val newsArticleService: NewsArticleService,
     private val ploggingEventService: PloggingEventService,
