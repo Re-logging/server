f8d2a49337bfb49fadcdbac518648435e94c6d53
Ares-10 <cholionking1@naver.com>
2024-10-21 04:42:03 +0900

    add: @EnableScheduling 추

--- src/main/kotlin/com/relogging/server/global/config/JpaConfig.kt
+++ src/main/kotlin/com/relogging/server/global/config/JpaConfig.kt
@@ -2,7 +2,9 @@ package com.relogging.server.global.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @Configuration
 @EnableJpaAuditing
+@EnableScheduling
 class JpaConfig
