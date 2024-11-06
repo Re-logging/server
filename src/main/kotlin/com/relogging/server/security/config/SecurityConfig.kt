422a01a1e813e85b2be744ae11d2c57d0a1c3aec
sinryuji <siler2474@gmail.com>
2024-11-06 19:28:34 +0900

    fix: oauth redirect rror

--- src/main/kotlin/com/relogging/server/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/relogging/server/security/config/SecurityConfig.kt
@@ -1,12 +1,12 @@
 package com.relogging.server.security.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.relogging.server.security.filter.LoggingFilter
+import com.relogging.server.security.filter.AuthenticationExceptionFilter
 import com.relogging.server.security.handler.JwtAccessDeniedHandler
 import com.relogging.server.security.handler.JwtAuthenticationEntryPoint
+import com.relogging.server.security.jwt.filter.JwtFilter
 import com.relogging.server.security.jwt.provider.TokenProvider
 import com.relogging.server.security.oauth.handler.OAuthAuthenticationSuccessHandler
-import com.relogging.server.security.oauth.repository.HttpCookieOAuth2AuthorizationRequestRepository
 import com.relogging.server.security.oauth.service.CustomOAuthUserService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -16,8 +16,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
@@ -26,15 +26,15 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 @EnableWebSecurity
 class SecurityConfig(
     @Value("\${cors-allow-url.front}")
-    private val frontUrl: String,
+    private var frontUrl: String,
+    @Value("\${cors-allow-url.local}")
+    private var localUrl: String,
     private val customOAuthUserService: CustomOAuthUserService,
     private val oAuthAuthenticationSuccessHandler: OAuthAuthenticationSuccessHandler,
     private val tokenProvider: TokenProvider,
     private val jwtAccessDeniedHandler: JwtAccessDeniedHandler,
     private val jwtAuthenticationEntryPoint: JwtAuthenticationEntryPoint,
     private val objectMapper: ObjectMapper,
-    private val httpCookieOAuth2AuthorizationRequestRepository: HttpCookieOAuth2AuthorizationRequestRepository,
-    private val loggingFilter: LoggingFilter,
 ) {
     @Bean
     @Throws(Exception::class)
@@ -53,12 +53,12 @@ class SecurityConfig(
                 authorize(HttpMethod.GET, "/api/ploggingMeetups/**", permitAll)
                 authorize(anyRequest, authenticated)
             }
-//            exceptionHandling {
-//                authenticationEntryPoint = jwtAuthenticationEntryPoint
-//                accessDeniedHandler = jwtAccessDeniedHandler
-//            }
-//            addFilterBefore<UsernamePasswordAuthenticationFilter>(JwtFilter(tokenProvider))
-//            addFilterBefore<JwtFilter>(AuthenticationExceptionFilter(objectMapper))
+            exceptionHandling {
+                authenticationEntryPoint = jwtAuthenticationEntryPoint
+                accessDeniedHandler = jwtAccessDeniedHandler
+            }
+            addFilterBefore<UsernamePasswordAuthenticationFilter>(JwtFilter(tokenProvider))
+            addFilterBefore<JwtFilter>(AuthenticationExceptionFilter(objectMapper))
             oauth2Login {
                 userInfoEndpoint { userService = customOAuthUserService }
                 authenticationSuccessHandler = oAuthAuthenticationSuccessHandler
@@ -66,7 +66,6 @@ class SecurityConfig(
 //                    authorizationRequestRepository = httpCookieOAuth2AuthorizationRequestRepository
 //                }
             }
-            addFilterBefore<OAuth2AuthorizationRequestRedirectFilter>(loggingFilter)
         }
 
         return http.build()
@@ -75,7 +74,7 @@ class SecurityConfig(
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf(*frontUrl.split(",").toTypedArray())
+        configuration.allowedOrigins = setOf(frontUrl, localUrl).toList()
         configuration.allowedMethods = listOf("POST", "GET", "DELETE", "PUT")
         configuration.allowedHeaders = listOf("*")
         configuration.allowCredentials = true
