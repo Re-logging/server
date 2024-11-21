0a4998404717ff7827d6d34cb6a64e93d18b666f
sinryuji <siler2474@gmail.com>
2024-11-21 18:46:44 +0900

    fix: profile update

--- src/main/kotlin/com/relogging/server/domain/user/dto/UpdateProfileRequest.kt
+++ src/main/kotlin/com/relogging/server/domain/user/dto/UpdateProfileRequest.kt
@@ -1,10 +1,8 @@
 package com.relogging.server.domain.user.dto
 
 import jakarta.validation.constraints.NotBlank
-import org.springframework.web.multipart.MultipartFile
 
 data class UpdateProfileRequest(
     @field:NotBlank
     val nickname: String,
-    val image: MultipartFile?,
 )
