e3e091b349b7f3007675638a19376f1cf105f23d
sinryuji <siler2474@gmail.com>
2024-11-22 17:47:28 +0900

    fix: token validate status code

--- src/main/kotlin/com/relogging/server/global/exception/GlobalErrorCode.kt
+++ src/main/kotlin/com/relogging/server/global/exception/GlobalErrorCode.kt
@@ -40,7 +40,7 @@ enum class GlobalErrorCode(
 
     // JWT 관련
     JWT_INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "JWT-001", "유효하지 않는 토큰입니다."),
-    JWT_MALFORMED_TOKEN(HttpStatus.BAD_REQUEST, "JWT-002", "잘못된 형식의 토큰입니다."),
+    JWT_MALFORMED_TOKEN(HttpStatus.UNAUTHORIZED, "JWT-002", "잘못된 형식의 토큰입니다."),
     JWT_EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "JWT-003", "유효기간이 만료된 토큰입니다."),
     JWT_UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "JWT-004", "지원하지 않는 형식의 토큰입니다."),
 
