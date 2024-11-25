11c61841e4a1cc01b20f473927edeeda77d2f1d6
sinryuji <siler2474@gmail.com>
2024-11-25 21:13:56 +0900

    test: cron 30 seconds

--- src/main/kotlin/com/relogging/server/infrastructure/scheduler/PloggingEventScheduler.kt
+++ src/main/kotlin/com/relogging/server/infrastructure/scheduler/PloggingEventScheduler.kt
@@ -1,23 +1,26 @@
 package com.relogging.server.infrastructure.scheduler
 
 import com.relogging.server.domain.plogging.service.PloggingEventService
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 class PloggingEventScheduler(
     private val ploggingEventService: PloggingEventService,
 ) {
-//    @Transactional
+    @Transactional
 //    @Scheduled(cron = "0 30 3 * * *") // 매일 오전 3시 30분에 작업 수행
-//    fun fetchAndSavePloggingEvent() {
-//        this.ploggingEventService.fetchPloggingEventList("플로깅")
-//            .then(this.ploggingEventService.fetchPloggingEventList("줍깅"))
-//            .subscribe()
-//    }
+    @Scheduled(cron = "*/30 * * * * *") // 매일 오전 3시 30분에 작업 수행
+    fun fetchAndSavePloggingEvent() {
+        this.ploggingEventService.fetchPloggingEventList("플로깅")
+            .then(this.ploggingEventService.fetchPloggingEventList("줍깅"))
+            .subscribe()
+    }
 
-//    @Transactional
-//    @Scheduled(cron = "0 40 3 * * *") // 매일 오전 3시 40분에 작업 수행
-//    fun deleteExpiredPloggingEvents() {
-//        this.ploggingEventService.deleteExpiredPloggingEvents()
-//    }
+    @Transactional
+    @Scheduled(cron = "0 40 3 * * *") // 매일 오전 3시 40분에 작업 수행
+    fun deleteExpiredPloggingEvents() {
+        this.ploggingEventService.deleteExpiredPloggingEvents()
+    }
 }
