aebba8365d9bb3ffb63d5deebfacf9dd378f3861
sinryuji <siler2474@gmail.com>
2024-12-05 17:28:40 +0900

    feat: UserResponse add userId

--- src/main/kotlin/com/relogging/server/domain/user/dto/UserConverter.kt
+++ src/main/kotlin/com/relogging/server/domain/user/dto/UserConverter.kt
@@ -5,6 +5,7 @@ import com.relogging.server.domain.user.entity.User
 object UserConverter {
     fun toResponse(user: User) =
         UserResponse(
+            userId = user.id,
             name = user.name,
             email = user.email,
             nickname = user.nickname,
--- src/main/kotlin/com/relogging/server/domain/user/dto/UserResponse.kt
+++ src/main/kotlin/com/relogging/server/domain/user/dto/UserResponse.kt
@@ -1,6 +1,7 @@
 package com.relogging.server.domain.user.dto
 
 data class UserResponse(
+    val userId: Long?,
     val name: String,
     val nickname: String,
     val email: String,
