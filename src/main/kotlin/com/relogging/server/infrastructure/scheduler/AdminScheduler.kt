32f91c46f87f867457b6f6f73f6a3bd52d5c6703
Ares-10 <cholionking1@naver.com>
2024-12-17 22:28:41 +0900

    feat: 관리자 카카오 메모 및 토큰 리프레시 스케줄링

--- src/main/kotlin/com/relogging/server/infrastructure/scheduler/AdminScheduler.kt
+++ src/main/kotlin/com/relogging/server/infrastructure/scheduler/AdminScheduler.kt
@@ -1,6 +1,47 @@
 package com.relogging.server.infrastructure.scheduler
 
+import com.relogging.server.infrastructure.admin.service.AdminAuthService
+import com.relogging.server.infrastructure.admin.service.AdminService
+import com.relogging.server.infrastructure.kakao.service.KakaoMessageService
+import org.springframework.scheduling.annotation.Async
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
-class AdminScheduler()
+class AdminScheduler(
+    private val adminService: AdminService,
+    private val adminAuthService: AdminAuthService,
+    private val kakaoMessageService: KakaoMessageService,
+) {
+    @Scheduled(cron = "0 0 8 * * *") // 매일 오전 8시
+    fun sendMorningMessage() {
+        val admins = adminService.findAll()
+        admins.forEach { admin ->
+            kakaoMessageService.sendMemo(
+                accessToken = admin.accessToken,
+                message = "좋은 아침입니다!",
+            )
+        }
+    }
+
+    @Scheduled(cron = "0 0 18 * * *") // 매일 오후 6시
+    @Async
+    fun sendEveningMessage() {
+        val admins = adminService.findAll()
+        admins.forEach { admin ->
+            kakaoMessageService.sendMemo(
+                accessToken = admin.accessToken,
+                message = "좋은 저녁입니다!",
+            )
+        }
+    }
+
+    @Scheduled(cron = "0 0 */1 * * *") // 매 시간마다
+    @Async
+    fun refreshAdminTokens() {
+        val admins = adminService.findAll()
+        admins.forEach { admin ->
+            adminAuthService.kakaoTokenRefresh(admin)
+        }
+    }
+}
