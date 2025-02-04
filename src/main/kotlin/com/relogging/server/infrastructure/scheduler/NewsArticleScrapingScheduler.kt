8a2881d9fb479726ce2ce70131f3d4a85fa7a522
Ares-10 <cholionking1@naver.com>
2025-02-05 01:37:16 +0900

    fix: 뉴스 스크래핑 및 ai요약 예외처리

--- src/main/kotlin/com/relogging/server/infrastructure/scheduler/NewsArticleScrapingScheduler.kt
+++ src/main/kotlin/com/relogging/server/infrastructure/scheduler/NewsArticleScrapingScheduler.kt
@@ -2,6 +2,8 @@ package com.relogging.server.infrastructure.scheduler
 
 import com.relogging.server.domain.newsArticle.entity.NewsArticle
 import com.relogging.server.domain.newsArticle.service.NewsArticleService
+import com.relogging.server.infrastructure.admin.service.AdminService
+import com.relogging.server.infrastructure.kakao.service.KakaoMessageService
 import com.relogging.server.infrastructure.openai.service.OpenAiService
 import com.relogging.server.infrastructure.scraping.service.NewsArticleScrapingService
 import org.slf4j.LoggerFactory
@@ -14,29 +16,39 @@ class NewsArticleScrapingScheduler(
     private val newsArticleScrapingService: NewsArticleScrapingService,
     private val newsArticleService: NewsArticleService,
     private val aiService: OpenAiService,
+    private val kakaoMessageService: KakaoMessageService,
+    private val adminService: AdminService,
 ) {
     private val log = LoggerFactory.getLogger(javaClass)
 
     @Scheduled(cron = "0 0 3 * * *") // 매일 오전 3시에 작업 수행
     @Transactional
     fun scrapNewsArticlesDaily() {
-        try {
-            log.info("Starting daily news scraping...")
-            val newsArticleList = mutableListOf<NewsArticle>()
+        log.info("Starting daily news scraping...")
+        val newsArticleList = mutableListOf<NewsArticle>()
 
-            // 뉴스 스크래핑 수행
+        // 뉴스 스크래핑 수행
+        try {
             newsArticleList.addAll(newsArticleScrapingService.scrapEconomyNews())
             newsArticleList.addAll(newsArticleScrapingService.scrapESGEconomy())
-
-            // AI 요약 생성
-            log.info("Generating AI summaries for ${newsArticleList.size} articles...")
-            newsArticleList.map { it.aiSummary = aiService.aiSummary(it.content) }
-
-            // DB 저장
-            val savedCount = newsArticleService.saveNewsArticles(newsArticleList)
-            log.info("Successfully scraped and saved $savedCount news articles")
         } catch (e: Exception) {
-            log.error("Failed to scrap news articles: ${e.message}", e)
+            adminService.findAll().forEach {
+                kakaoMessageService.sendMemo(it.accessToken, "뉴스 스크래핑 중 오류 발생: ${e.message}")
+            }
         }
+
+        // AI 요약 생성
+        log.info("Generating AI summaries for ${newsArticleList.size} articles...")
+        try {
+            newsArticleList.map { it.updateAiSummary(aiService.aiSummary(it.content)) }
+        } catch (e: Exception) {
+            adminService.findAll().forEach {
+                kakaoMessageService.sendMemo(it.accessToken, "뉴스 AI 요약 중 오류 발생: ${e.message}")
+            }
+        }
+
+        // DB 저장
+        val savedCount = newsArticleService.saveNewsArticles(newsArticleList)
+        log.info("Successfully scraped and saved $savedCount news articles")
     }
 }
